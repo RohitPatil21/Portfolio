2d3ef4a62bd4e82abb824b1b2b5c91c72fd10c94
Rohit Patil <rohitpatil7@yahoo.com>
2022-11-23 13:47:52 +0530

    worked on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "../styles/navbar.scss";
+import "../styles/common.scss";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <h1 className="navbar__heading"> Hello guys </h1>
+        <h1 className="navbar__heading text-center"> Hello guys </h1>
     </div>
   )
 }
