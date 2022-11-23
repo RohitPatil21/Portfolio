7c2b9e0e6b359a0c36011b6b9c9140c36d014096
Rohit Patil <rohitpatil7@yahoo.com>
2022-11-23 15:13:04 +0530

    navbar and hello

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
-
+import "../styles/landing.scss";
 
 const Landing = () => {
     return(
         <div className='landing'>
            <Navbar />
+           <h1> Hello </h1>
         </div>
     )
 }
