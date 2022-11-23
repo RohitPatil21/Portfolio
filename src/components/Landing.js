259608782a11aa8b2cbbfc1b9a32d20209f7d4ec
Rohit Patil <rohitpatil7@yahoo.com>
2022-11-23 13:19:51 +0530

    just demo

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Landing = () => {
+    return(
+        <div className='landing'>
+            <h1> welcome to lading page </h1>
+        </div>
+    )
+}
+
+export default Landing;
